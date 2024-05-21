3ec74e01782abcaea0a0702402928f682ea85aa4
Marcauch <marcauch@yahoo.com>
2024-05-20 22:17:01 -0500

    Update pong-classes.js

--- javascript/pong-classes.js
+++ javascript/pong-classes.js
@@ -44,7 +44,7 @@ export class Ball {
     this.acceleration = 0; //X speed multipler on successful block
     this.initial = { x: 2, y: 0 }; //speed reset on point score
     this.maximum = { x: 2, y: 1 }; //maximum speed
-    this.inertialTransfer = 0.25; //multiplier for velocity.y transfer from paddle
+    this.inertialTransfer = 0.50; //multiplier for velocity.y transfer from paddle
     this.htmlElement = null; //element on HTML page
   }
 }
@@ -65,7 +65,7 @@ export class Paddle {
     this.oldPosition = { x: 50, y: 50 }; //paddle position 1 frame ago
     this.velocity = { x: 0, y: 0 }; //calculated velocity
     this.direction = { x: 0, y: 0 }; //position offset
-    this.speed = 20; //rate that paddle moves with keyboard
+    this.speed = 10; //rate that paddle moves with keyboard
     this.hasHit = false; //keep track of wheter paddle hit ball or not
     this.htmlElement = null; //element on HTML page
   }
